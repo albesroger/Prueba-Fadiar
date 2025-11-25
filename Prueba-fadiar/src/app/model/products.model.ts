4d1d5f52eeab011fa507ee0f528889cb6a13f48f
albesroger <albescaballero@gmail.com>
2025-11-25 03:31:40 -0500

    haciendo el grid

---
+++ Prueba-fadiar/src/app/model/products.model.ts
@@ -0,0 +1,40 @@
+// producto.ts
+export interface Product {
+  id: number;
+  cantidad?: number;
+  name: string;
+  img: string;
+  description: string;
+  model: string;
+  brand: string;
+  regret: boolean;
+  disabled: boolean;
+  pre_venta: boolean;
+  warranty: number;
+  price: number;
+  temporal_price: number;
+  commission: number;
+  count: number;
+  specs: [{ id: number; name: string; description: string }];
+  categoria: [{ id: number; name: string; id_padre: number | null }];
+  size: [
+    {
+      id: number;
+      size: string;
+      units: number;
+      mode: string;
+      price_increment: number;
+      delivery_commission: number;
+      id_commission_currency: number;
+    }
+  ];
+  currency: [
+    {
+      id: number;
+      main: boolean;
+      value: number;
+      active: boolean;
+      currency: string;
+    }
+  ];
+}
