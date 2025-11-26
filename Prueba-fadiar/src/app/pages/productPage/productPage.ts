2eb941f9367c14202513dcedf119b07da43a5c91
albesroger <albescaballero@gmail.com>
2025-11-26 05:13:21 -0500

    faqPage terminada

--- Prueba-fadiar/src/app/pages/productPage/productPage.ts
+++ Prueba-fadiar/src/app/pages/productPage/productPage.ts
@@ -2,19 +2,29 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 import { Welcome } from '../../components/welcome/welcome';
-import { SidebarFilterComponent, SidebarFilters } from '../../components/sidebarFilter/sidebarFilter';
+import {
+  SidebarFilterComponent,
+  SidebarFilters,
+} from '../../components/sidebarFilter/sidebarFilter';
 import { ProductsGrid } from '../../components/products-grid/products-grid';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../model/products.model';
 import { ProductSearchService } from '../../services/product-search.service';
 import { Observable } from 'rxjs';
-import { Billetes } from "../../components/billetes/billetes";
-import { UltimosProductos } from "../../components/ultimosProductos/ultimosProductos";
+import { Billetes } from '../../components/billetes/billetes';
+import { UltimosProductos } from '../../components/ultimosProductos/ultimosProductos';
 
 @Component({
   selector: 'app-product-page',
   standalone: true,
-  imports: [CommonModule, Welcome, SidebarFilterComponent, ProductsGrid, Billetes, UltimosProductos],
+  imports: [
+    CommonModule,
+    Welcome,
+    SidebarFilterComponent,
+    ProductsGrid,
+    Billetes,
+    UltimosProductos,
+  ],
   templateUrl: './productPage.html',
 })
 export class ProductPage implements OnInit {
