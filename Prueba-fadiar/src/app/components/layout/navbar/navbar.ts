25e4e1788f77beafd873f6b8303d1f3ca807ac52
albesroger <albescaballero@gmail.com>
2025-11-27 16:14:20 -0500

    responsive hecho

--- Prueba-fadiar/src/app/components/layout/navbar/navbar.ts
+++ Prueba-fadiar/src/app/components/layout/navbar/navbar.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { ProductSearchService } from '../../../services/product-search.service';
-import { ProductSearch } from '../../product-search/product-search';
 import { CartService } from '../../../services/cart.service';
 import { Observable } from 'rxjs/internal/Observable';
+import { ProductSearch } from '../../product-search/product-search';
 
 @Component({
   selector: 'app-navbar',
@@ -13,12 +13,14 @@ import { Observable } from 'rxjs/internal/Observable';
     RouterLink,
     FormsModule,
     CommonModule,
-    ProductSearch,
     RouterLinkActive,
+    ProductSearch,
   ],
   templateUrl: './navbar.html',
 })
 export class Navbar {
+  mobileMenuOpen = false;
+  searchTerm = '';
   cartCount$: Observable<number>;
   constructor(
     private productSearchService: ProductSearchService,
@@ -28,8 +30,29 @@ export class Navbar {
     this.cartCount$ = this.cartService.cartCount$; // ✅ aquí
   }
 
+  navItems = [
+    { label: 'Inicio', url: '/', exact: true },
+    { label: 'Productos', url: '/product', exact: false },
+    { label: 'About Us', url: '/about', exact: false },
+    { label: 'FAQ', url: '/faq', exact: false },
+    { label: 'Contacto', url: '/contacto', exact: false },
+  ];
+
+  toggleMobileMenu() {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
+
+  closeMobileMenu() {
+    this.mobileMenuOpen = false;
+  }
+
   onTermChange(term: string) {
     this.productSearchService.setTerm(term);
+  }
+  onSearch() {
+    // aquí metes la lógica de búsqueda
+    // por ejemplo emitir un evento o navegar con query params
+    console.log('Buscar:', this.searchTerm);
   }
   goToCart() {
     this.router.navigate(['/cart']); // cambia '/cart' si tu ruta es otra
